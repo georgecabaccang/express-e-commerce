befdb8963c02a123ed2848d1001d58f7fc824211
georgesycabaccang <georgesycabaccang@gmail.com>
2024-06-20 17:49:59 +0800

    Create authentication middleware.

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/userModel";
+
+export const authentication = async (request: Request, response: Response, next: NextFunction) => {
+    const userId = request.params.userId;
+    const email = request.body.email;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        response.status(404).send({ status: 404, message: "user_not_found" });
+    } else {
+        request.body.user = user;
+
+        if (!userId) return next();
+        if (user._id.toString() !== userId) {
+            response.status(401).send({ status: 401, message: "invalid_user" });
+        } else {
+            next();
+        }
+    }
+};
